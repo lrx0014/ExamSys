a395d6b9170e0dfb377fb56c21d0a1bc3af633cf
lrx0014 <lrx0014@hotmail.com>
2018-01-30 17:58:41 +0800

    update connect.php

--- ExamSys/Support/action/connect.php
+++ ExamSys/Support/action/connect.php
@@ -3,13 +3,14 @@
 error_reporting(E_ALL & ~E_DEPRECATED);
 
 /// 此处可更改数据库用户
-$IP      = "127.0.0.1";
-$DB_USER = "Exam";
-$DB_PWD  = "exam1234";
+$IP      = "127.0.0.1";     // 数据库IP端口
+$DB_USER = "Exam";          // 用户名
+$DB_PWD  = "exam1234";      // 密码
+$DB_NAME = "Examdb";        // 数据库名
 
 $con = mysql_connect($IP, $DB_USER, $DB_PWD);
 mysql_query("set names 'utf8'",$con);
-$dbLink = mysql_select_db("Examdb",$con);
+$dbLink = mysql_select_db("$DB_NAME,$con);
 
 
 if (!$con) {
